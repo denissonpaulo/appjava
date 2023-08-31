f522c368d92efd2dbf8d6b3a9e29123a5b44ea50
Denisson <68811724+denissonpaulo@users.noreply.github.com>
2023-08-31 11:52:21 -0300

    First test

---
+++ first.java
@@ -0,0 +1,9 @@
+/* This is first programming JAVA, example comments */
+
+class first {
+    //call main() , example comments in line
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
